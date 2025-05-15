a56c512d08c6255e43a6de8507ef2428dc6f50b5
Armaan Irani <armaanirani@gmail.com>
2025-05-15 13:00:44 +0530

    added buttons to the app

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ st.set_page_config(
 )
 
 st.title("Template App for Gen AI Applications")
+
+col1, col2 = st.columns(2)
+
+if col1.button("Check Server Health"):
+    st.success("Server is healthy!")
+
+
+if col2.button("List LLMs"):
+    st.success("Server is healthy!!")
