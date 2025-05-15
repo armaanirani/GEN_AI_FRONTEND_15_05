1c758666946bff22edc317f9012cfd7487b557ec
Armaan Irani <armaanirani@gmail.com>
2025-05-15 13:15:36 +0530

    added function logic to streamlit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from utils.config import app_config, env_config
 from utils.logger import logger
+from services.check_server_health import test_health
+from services.get_llms_list import get_llms_list
 
 st.set_page_config(
     page_title="Test App",
@@ -13,8 +15,14 @@ st.title("Template App for Gen AI Applications")
 col1, col2 = st.columns(2)
 
 if col1.button("Check Server Health"):
-    st.success("Server is healthy!")
+    logger.info("Checking server health...")
+    result = test_health()
+    logger.info(f"Server health check result: {result}")
+    st.json(result)
 
 
 if col2.button("List LLMs"):
-    st.success("Server is healthy!!")
+    logger.info("Fetching LLMs list...")
+    list_of_llms = get_llms_list()
+    logger.info(f"LLMs list retrieval result: {list_of_llms}")
+    st.json(list_of_llms)
